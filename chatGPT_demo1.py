3683dfa6ea8c32abc95eaef6b7a137fe896f4500
mailcz8 <25183551+mailcz8@users.noreply.github.com>
2023-04-25 09:51:27 -0700

    first script

---
+++ chatGPT_demo1.py
@@ -0,0 +1,16 @@
+import os
+import openai
+
+openai.api_key = os.getenv("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")
+
+print("this: ", openai.api_key)
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="Create a SQL request to find all users who live in California and have over 1000 credits:",
+  temperature=0.3,
+  max_tokens=60,
+  top_p=1.0,
+  frequency_penalty=0.0,
+  presence_penalty=0.0
+)
